0aceeeae984b699e9521c16ef946a525b4013ca1
Luis Flores <lflores@proxlearn.com>
2022-04-10 00:01:55 -0500

    Parametize user account in show method

--- lib/getcanvasuser.rb
+++ lib/getcanvasuser.rb
@@ -10,7 +10,13 @@ module Getcanvasuser
   class Error < StandardError; end
   # Your code goes here...
   def self.show(userID)
-    get("/api/v1/users/#{userID}.json")
+    #get("/api/v1/users/#{userID}.json")
+    #res = HTTParty.get('https://canvas.instructure.com/api/v1/users/34172232/profile', headers: { 'Authorization' => "Bearer 7~qIWgJj4c47cqg6RR5cFDsMYhlWEKDu4c2F7qpeVA17PSpRSdZC0EVkXTG3K3bXYb" })
+    res = HTTParty.get("https://canvas.instructure.com/api/v1/users/#{userID}/profile", 
+                      headers: { 'Authorization' => "Bearer 7~qIWgJj4c47cqg6RR5cFDsMYhlWEKDu4c2F7qpeVA17PSpRSdZC0EVkXTG3K3bXYb" })
+    usuario = JSON.parse(res.body)
+    #puts usuario.inspect
+    puts usuario["sortable_name"]
   end
 end
 
